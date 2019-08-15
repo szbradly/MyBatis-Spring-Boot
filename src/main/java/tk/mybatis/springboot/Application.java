281e92d48b3b6727eff4406390dcb454ce2f6f6f
szjack <510616545@qq.com>
2019-08-15 19:13:49 +0800

    modify

--- src/main/java/tk/mybatis/springboot/Application.java
+++ src/main/java/tk/mybatis/springboot/Application.java
@@ -4,27 +4,23 @@ package tk.mybatis.springboot;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 import tk.mybatis.spring.annotation.MapperScan;
 
 /**
- * @author liuzh
- * @since 2015-12-12 18:22
+ * @author jack
+ * @since 2019-08-15 18:55
  */
 @SpringBootApplication
 @MapperScan(basePackages = "tk.mybatis.springboot.mapper")
-public class SpringApplication  {
+public class Application {
     private Logger logger = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-
+  
 }
--- src/main/java/tk/mybatis/springboot/controller/CityController.java
+++ src/main/java/tk/mybatis/springboot/controller/CityController.java
@@ -24,17 +24,20 @@
 
 package tk.mybatis.springboot.controller;
 
+import java.util.List;
+
 import com.github.pagehelper.PageInfo;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 import tk.mybatis.springboot.model.City;
 import tk.mybatis.springboot.service.CityService;
-
-import java.util.List;
 
 /**
  * @author liuzh
