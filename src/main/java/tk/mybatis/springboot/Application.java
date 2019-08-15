f4a129e110d7e4ea16fdb357379a6264a6fadb89
szjack <510616545@qq.com>
2019-08-15 18:53:00 +0800

    upgrade

--- src/main/java/tk/mybatis/springboot/Application.java
+++ src/main/java/tk/mybatis/springboot/Application.java
@@ -17,24 +17,14 @@ import tk.mybatis.spring.annotation.MapperScan;
  * @author liuzh
  * @since 2015-12-12 18:22
  */
-@Controller
-@EnableWebMvc
 @SpringBootApplication
 @MapperScan(basePackages = "tk.mybatis.springboot.mapper")
-public class Application extends WebMvcConfigurerAdapter implements CommandLineRunner {
+public class SpringApplication  {
     private Logger logger = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        logger.info("服务启动完成!");
-    }
 
-    @RequestMapping("/")
-    String home() {
-        return "redirect:countries";
-    }
 }
