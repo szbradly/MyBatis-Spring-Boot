ff924e063ee61059ef12ab62bda1fa074d56de0f
szjack <510616545@qq.com>
2019-08-15 19:19:38 +0800

    get run

--- src/main/java/tk/mybatis/springboot/Application.java
+++ src/main/java/tk/mybatis/springboot/Application.java
@@ -1,10 +1,7 @@
 package tk.mybatis.springboot;
 
-//特别注意，下面的是 tk.MapperScan
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import tk.mybatis.spring.annotation.MapperScan;
@@ -13,10 +10,10 @@ import tk.mybatis.spring.annotation.MapperScan;
  * @author jack
  * @since 2019-08-15 18:55
  */
+@EnableAutoConfiguration 
 @SpringBootApplication
 @MapperScan(basePackages = "tk.mybatis.springboot.mapper")
 public class Application {
-    private Logger logger = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
